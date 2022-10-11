fb5eedc4df17772d633206a218a726aaa0eba26f
eliscmatt <eliscmattosinho@gmail.com>
2022-10-11 19:17:03 -0300

    doc: limpeza nos arquivos

--- Matriz/matrizz.cpp
+++ Matriz/matrizz.cpp
@@ -1,1 +1,35 @@
 #include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+int A, p, q, B, q, r;
+
+}
+
+int multiplicacaoMatrizes (int p, int q, int A, int B, int r) {
+    if(int p == int q) {
+        
+    }
+}
+
+//p n linhas matriz A 
+//q n linhas matriz B
+
+// As ?p? linhas seguintes à primeira recebem ?q? números inteiros.
+
+
+// Se A é uma matriz m×n e B é uma matriz n×p, então seu produto é uma matriz m×p definida como AB.
+
+// A pxq . B qxr = AB (p, r)
+
+//p+1 recebe 2 valores
+
+
+
+
+
+// o entrada duas matrizes Ap,q e Bq,r e como resultado produzirá uma matriz Cp,r . T
+
+// produto de duas matrizes é definido somente quando o número de colunas da primeira matriz é igual ao número de linhas da segunda matriz
